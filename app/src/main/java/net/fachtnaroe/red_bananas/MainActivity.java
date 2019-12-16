257298b09185f244234971985e68c7025fbb8beb
Ryker-Smith <fachtnaroe@gmail.com>
2019-12-16 16:16:21 +0000

    _Login_ screen *looks* correct.

--- app/src/main/java/net/fachtnaroe/red_bananas/MainActivity.java
+++ app/src/main/java/net/fachtnaroe/red_bananas/MainActivity.java
@@ -31,28 +31,24 @@ public class MainActivity extends Form implements HandlesEventDispatching {
     private String passwordForURL = "", weblogin = "https://fachtnaroe.net/bananas?cmd=LOGIN&user=", webLogin2 = "&pass=", ResonseContent;
     private Web webLoginConnection;
     private Notifier GotTextNotifier;
-    boolean buyerBool = false, salesBool = false;
     private static String usernameForURL = "", SessionId = "", pID = "";
 
     protected void $define() {
+        this.BackgroundColor(Component.COLOR_ORANGE);
         webLoginConnection = new Web(this);
-
         GotTextNotifier = new Notifier(this);
         GotTextNotifier.BackgroundColor(Component.COLOR_BLACK);
-        GotTextNotifier.TextColor(Component.COLOR_PINK);
-
         Varr1 = new VerticalArrangement(this);
         Varr1.Height(LENGTH_FILL_PARENT);
         Varr1.Width(LENGTH_FILL_PARENT);
-        Varr1.BackgroundColor(COLOR_PINK);
-
+        Varr1.Image("FDS_PossibleLogo_03.png");
         title = new Label(Varr1);
         title.Width(LENGTH_FILL_PARENT);
-        title.HeightPercent(20);
+//        title.HeightPercent(20);
         title.Text("Food Delivery Service");
         title.TextColor(COLOR_BLACK);
         title.FontBold(true);
-        title.FontSize(30);
+        title.FontSize(60);
         title.TextAlignment(Component.ALIGNMENT_CENTER);
 
         Harr_EmptySpaceAfterTitle = new HorizontalArrangement(Varr1);
@@ -63,12 +59,14 @@ public class MainActivity extends Form implements HandlesEventDispatching {
         Harr_UserInfo.HeightPercent(13);
 
         UserL = new Label(Harr_UserInfo);
-        UserL.Text("Username");
+        UserL.Text("Username: ");
         UserL.WidthPercent(30);
+        UserL.TextAlignment(ALIGNMENT_OPPOSITE);
 
         username = new TextBox(Harr_UserInfo);
         username.Width(LENGTH_FILL_PARENT);
         username.Text("redshop");
+        username.BackgroundColor(Component.COLOR_WHITE);
 
         Harr_Password = new HorizontalArrangement(Varr1);
         Harr_Password.Width(LENGTH_FILL_PARENT);
@@ -76,21 +74,25 @@ public class MainActivity extends Form implements HandlesEventDispatching {
 
         PasswL = new Label(Harr_Password);
         PasswL.WidthPercent(30);
-        PasswL.Text("Password");
+        PasswL.Text("Password: ");
         PasswL.TextColor(COLOR_BLACK);
+        PasswL.TextAlignment(ALIGNMENT_OPPOSITE);
+
 
         password = new PasswordTextBox(Harr_Password);
         password.Width(LENGTH_FILL_PARENT);
         password.Text("tcfetcfe");
+        password.BackgroundColor(Component.COLOR_WHITE);
 
         Harr_Checkboxes = new HorizontalArrangement(Varr1);
         Harr_Checkboxes.Width(LENGTH_FILL_PARENT);
         Harr_Checkboxes.HeightPercent(20);
 
-         H1_EmptySpaceBefore = new HorizontalArrangement(Harr_Checkboxes);
+        H1_EmptySpaceBefore = new HorizontalArrangement(Harr_Checkboxes);
         H1_EmptySpaceBefore.WidthPercent(20);
 
         buyer = new CheckBox(Harr_Checkboxes);
+        buyer.Checked(true);
         buyer.Text("Buyer");
         buyer.TextColor(COLOR_BLACK);
         buyer.FontSize(25);
@@ -100,6 +102,7 @@ public class MainActivity extends Form implements HandlesEventDispatching {
         seller.Text("Seller");
         seller.TextColor(COLOR_BLACK);
         seller.FontSize(25);
+        seller.Checked(false);
 
         H2_EmptySpaceAfter = new HorizontalArrangement(Harr_Checkboxes);
         H2_EmptySpaceAfter.WidthPercent(20);
@@ -113,8 +116,8 @@ public class MainActivity extends Form implements HandlesEventDispatching {
         login.Text("Login");
         login.FontSize(25);
         login.TextAlignment(Component.ALIGNMENT_CENTER);
-        login.TextColor(Component.COLOR_PINK);
-        login.BackgroundColor(COLOR_BLACK);
+        login.BackgroundColor(Component.COLOR_NONE);
+        login.FontBold(true);
 
         EventDispatcher.registerEventForDelegation(this, formName, "Click");
         EventDispatcher.registerEventForDelegation(this, formName, "GotText");
@@ -128,16 +131,17 @@ public class MainActivity extends Form implements HandlesEventDispatching {
                 return true;
             }
         }
-
-        if (component.equals(buyer) && eventName.equals("Changed")) {
-            buyerBool = !buyerBool;
-            return true;
+        else  if (eventName.equals("Changed")) {
+            if (component.equals(buyer)) {
+                seller.Checked( !buyer.Checked() );
+                return true;
+            }
+            else if (component.equals(seller)) {
+                buyer.Checked(!seller.Checked());
+                return true;
+            }
         }
-        if (component.equals(seller) && eventName.equals("Changed")) {
-            salesBool = !salesBool;
-            return true;
-        }
-        if (eventName.equals("GotText")) {
+        else if (eventName.equals("GotText")) {
             jsonLoginCheckAndValuePasser(params[1].toString(),params[3].toString());
             return true;
         }
@@ -145,14 +149,10 @@ public class MainActivity extends Form implements HandlesEventDispatching {
     }
 
     public void loginBtnClick() {
-        if ((salesBool && buyerBool) || (!(salesBool || buyerBool))) {
-            GotTextNotifier.ShowAlert("Check ONE box");
-        } else {
             usernameForURL = username.Text();
             passwordForURL = password.Text();
             webLoginConnection.Url(weblogin + usernameForURL + webLogin2 + passwordForURL);
             webLoginConnection.Get();
-        }
     }
     //this procedure can be called for both listViews, (Slightly Altered code I got from Fachtna that is more efficient than the previous code and uses the kawa-1.7 library)
     public void jsonLoginCheckAndValuePasser(String status, String textOfResponse) {
@@ -163,10 +163,11 @@ public class MainActivity extends Form implements HandlesEventDispatching {
             if(status_json.contains("OK")){
                 pID = parser.getString("pID");
                 SessionId = parser.getString("sessionID");
-                if (salesBool && !buyerBool) {
+                if (seller.Checked()) {
                     Intent i = new Intent(getApplicationContext(), Sales.class);
                     startActivity(i);
-                } else if (!salesBool && buyerBool) {
+                }
+                else {
                     Intent i = new Intent(getApplicationContext(), Order.class);
                     startActivity(i);
                 }
